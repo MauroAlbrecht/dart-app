24e731d0a3841cb08e41a1d5e71c37348522695d
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-09-06 15:09:12 -0300

    Commit inicial

---
+++ bin/calculadora_media_escolar_dart.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+import 'dart:io';
+
+/*void main(List<String> arguments) {
+  print("Informe a nota da prova 1");
+  var line = stdin.readLineSync(encoding: utf8);
+  var prova1 = int.parse(line ?? "0");
+
+  print("Informe a nota da prova 2");
+  line = stdin.readLineSync(encoding: utf8);
+  var prova2 = int.parse(line ?? "0");
+
+  print(prova1);
+  print(prova2);
+
+  var media = (prova1 + prova2) / 2;
+
+  if (media >= 7) {
+    print("O aluno passou com a nota: $media");
+  } else if (media >= 5) {
+    print("O aluno está de recuperação com a nota: $media");
+  } else {
+    print(("O aluno reprovou com a nota: $media"));
+  }
+}*/
