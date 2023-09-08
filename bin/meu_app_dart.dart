4971e072128a59ecf9c274d9f4151966e9930802
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-09-08 14:13:58 -0300

    ajuste de pastas

--- bin/meu_app_dart.dart
+++ bin/meu_app_dart.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'calculadora_media_escolar_dart.dart';
+import 'calculadora/calculadora_media_escolar_dart.dart';
 
 void main(List<String> arguments) {
   calculadora(arguments);
