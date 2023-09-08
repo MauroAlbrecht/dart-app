77dd70b58619ea9324b511d2854f703f8187721c
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-09-08 16:15:01 -0300

    alterado classe Pessoa para abstract

--- bin/meu_app_dart.dart
+++ bin/meu_app_dart.dart
@@ -3,8 +3,10 @@ import 'package:meu_app_dart/classes/pessoa_fisica.dart';
 import 'package:meu_app_dart/classes/pessoa_juridica.dart';
 
 void main(List<String> arguments) {
-  var pessoa = Pessoa("Mauro","Endereço");
-  print(pessoa.toString());
+
+  //classe abstrata não pode ser instanciada
+  //var pessoa = Pessoa("Mauro","Endereço");
+  //print(pessoa.toString());
 
   var pessoaFisica = PessoaFisica("Mauro","Endereço", "123456789");
   print(pessoaFisica.toString());
